29896da37a2bf81abc92b532d25e43ab21abb422
Ranju Shrestha <ranjustha8844@gmail.com>
2025-03-15 19:39:49 +0545

    added password visibility

--- my-react-app/src/LoginSignup.jsx
+++ my-react-app/src/LoginSignup.jsx
@@ -3,6 +3,15 @@ import { useState } from 'react'
 
  function LoginSignup() {
     const [isLogin, setIsLogin] = useState(true);
+    const [showPassword, setShowPassword] = useState(false); // State for password visibility
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword); // Toggle password visibility
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword); // Toggle confirm password visibility
+  };
   return (
     <div className='container'>
         <div className='form-container'>
@@ -13,8 +22,21 @@ import { useState } from 'react'
             {isLogin ? <>
             <div className="form">
                 <h2>Login</h2>
+                
                 <input type="email"  placeholder='Email'/>
-                <input type="password"  placeholder='Password'/>
+                <input
+              type={showPassword ? 'text' : 'password'} // Toggle password visibility
+              placeholder='Password'
+            />
+            <label className='show-password-label'>
+              <input
+                type='checkbox'
+                checked={showPassword}
+                onChange={togglePasswordVisibility}
+              />
+              Show Password
+            </label>
+                
                 <a href="#">Forgot Password?</a>
                 <button>Login</button>
                 <p>Not a member? <a href="#" onClick={() => setIsLogin(false)}>signup now</a></p>
@@ -24,8 +46,22 @@ import { useState } from 'react'
             <div className='form'>
             <h2>Signup</h2>
                 <input type="email"  placeholder='Email'/>
-                <input type="password"  placeholder='Password'/>
-                <input type="password"  placeholder='confirm Password'/>
+                <input
+              type={showPassword ? 'text' : 'password'} // Toggle password visibility
+              placeholder='Password'
+            />
+            <input
+              type={showPassword ? 'text' : 'password'} // Toggle confirm password visibility
+              placeholder='Confirm Password'
+            />
+            <label className='show-password-label'>
+              <input
+                type='checkbox'
+                checked={showPassword}
+                onChange={togglePasswordVisibility}
+              />
+              Show Password
+            </label>
                 <button>Signup</button>
                 </div>
                 </>}
